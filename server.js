72431d5b266191e3aefd05a3725bcbe3e86ea3a9
jsaldana777 <joel.saldana.cbsg@outlook.com>
2025-08-24 01:28:40 -0500

    Se creo el levantamiento de la API usando el puerto

---
+++ server.js
@@ -0,0 +1,20 @@
+
+// Levantar el servidor en el puerto 3000:
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+// Middleware para JSON:
+app.use(express.json());
+
+// Enviar un param a la BBDD.
+// Respond es lo que se obtiene.
+// IMPRIME EL MENSAJE EN PANTALLA, SOLO ES PARA QUE VEAMOS QUE SE PUEDE LEVANTAR EL API, NO TIENE NADA QUE VER CON EL FRONT-END.
+app.get('/', (req, res) => {
+    res.send('API FUNCIONANDO');
+});
+
+// Asignar un puerto para levantar la api.
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
